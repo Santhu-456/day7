7198b898f8347fcae7a14a42777ac3bb7d073534
Santhu-456 <117020682+Santhu-456@users.noreply.github.com>
2025-08-16 18:55:54 +0530

    Delete Desktop/day5/userstatus directory

--- Desktop/day5/userstatus/src/components/WithLogger.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const withLogger = (WrappedComponent) => {
-  return (props) => {
-    console.log("[HOC Log] Wrapped component rendered with props", props);
-    return <WrappedComponent {...props} />;
-  };
-};
-
-export default withLogger;
